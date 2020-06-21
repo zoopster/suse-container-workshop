e99007657b0833d29e0b184c72447530e623c729
dsohk <derek.so@suse.com>
2020-06-21 10:59:37 +0800

    remove unused files

--- cleangit.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -ex
-
-export REPO=git@github.com-suse:dsohk/suse-container-workshop.git
-rm -rf .git
-git init
-git add .
-git commit -m 'Lesson 1'
-git remote add origin $REPO
-git push --mirror --force
